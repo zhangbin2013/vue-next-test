4bc0f6e0e592f54ab4673501d28f2de172152661
zhangbin_yq <zhangbin_yq@kedacom.com>
2020-09-17 14:48:33 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div>窗口{{length}}</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -14,28 +15,33 @@ export default {
   components: {
     HelloWorld
   },
+  data(){
+    return {
+      length: 0
+    }
+  },
   directives: {
     demo: {
       // 只调用一次，指令第一次绑定到元素时调用
       // 在这里可以进行一次性的初始化设置 el.parentNode -> null
-      bind: (el, binding, vnode) => {},
+      bind(el, binding, vnode) {},
 
       // 被绑定元素插入父节点时调用   el.parentNode 访问当前节点的父节点
       // 仅保证父节点存在，但不一定已插入到文档中!!!
-      inserted: (el, binding, vnode) => {},
+      inserted(el, binding, vnode) {},
 
       // 所在组件的VNode 更新时调用
       // 但是可能发生在其子VNode更新之前
       // 指令的值可能发生了改变，也可能没有
       // 但是可以通过比较更新前后的值来忽略不必要的模板更新
-      update: (el, binding, vnode, oldVnode) => {},
+      update(el, binding, vnode, oldVnode){},
 
       // 指令所在在组件的VNode 及其子VNode 全部更新后调用
       // eslint-disable-next-line no-unused-vars
-      componentUpdated: (el, binding, vnode, oldVnode) => {},
+      componentUpdated(el, binding, vnode, oldVnode){},
 
       // 只调用一次，指令与元素解绑时调用
-      unbind: (el, binding, vnode) => {}
+      unbind(el, binding, vnode){}
     }
     /*function(
       // 指令绑定的元素，可以用来直接操作DOM
